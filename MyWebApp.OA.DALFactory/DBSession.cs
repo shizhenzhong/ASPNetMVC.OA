93a4901a981d4ea8b9fb18c40a1391ae35a5749d
Administrator <Administrator@PC-201810040931>
2018-10-09 17:48:39 +0800

    s

--- MyWebApp.OA.DALFactory/DBSession.cs
+++ MyWebApp.OA.DALFactory/DBSession.cs
@@ -13,7 +13,9 @@ namespace MyWebApp.OA.DALFactory
     /// DBsession:数据会话层，负责数据操作类实例的创建，然后业务层调用数据会话层，获取相应的数据操作类实例，
     /// DBSession:其实就是一个工厂类，完成了业务层与数据层的解耦
     /// </summary>
-   public class DBSession
+    /// 
+
+   public class DBSession:IDBSession
     {
         private IUserInfoDal _UserInfoDal;
         public IUserInfoDal UserInfoDal
---
+++ MyWebApp.OA.IDAL/IDBSession.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MyWebApp.OA.IDAL
+{
+    public interface IDBSession
+    {
+        IUserInfoDal UserInfo { get; set; }
+        bool SaveChanges();
+    }
+}
