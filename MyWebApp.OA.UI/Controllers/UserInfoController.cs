f26b394302150604f4e7687249e3011dac4d4226
Administrator <Administrator@PC-201810040931>
2018-10-10 16:00:04 +0800

    用户信息展示

---
+++ MyWebApp.OA.UI/Controllers/UserInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebApp.OA.UI.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        //
+        // GET: /UserInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
